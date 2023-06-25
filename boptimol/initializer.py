0b9d884c44dfc4f2942db26f2d66973d2184636e
Mayank Goel <mayank.goel447@gmail.com>
2023-06-25 07:25:07 +0530

    Structurized optimizer.py

--- boptimol/initializer.py
+++ boptimol/initializer.py
@@ -1,12 +1,13 @@
 from typing import List, Optional
+import logging
 
 import numpy as np
-
 from boptimol import Molecule
 
+logger = logging.getLogger(__name__)
 
 # Making initial guesses for the molecule (Active Learning)
-def initial_guess(mol: Molecule, init_steps: int) -> np.ndarray:
+def initial_guess(mol: Molecule, init_steps: int, out_dir: str) -> np.ndarray:
     
     ''' Return initial coordinates guess based on the molecule (Acti)
     Args:
@@ -17,4 +18,21 @@ def initial_guess(mol: Molecule, init_steps: int) -> np.ndarray:
     '''
     
     start_coords = mol.parameters
-    return np.random.normal(start_coords, 0.1, size=(init_steps, len(start_coords)))
+    start_energy = mol.energy(start_coords)
+    
+    init_guesses = np.random.normal(start_coords, 0.1, size=(init_steps, len(start_coords))) #TODO
+    init_energies = []
+    
+    # Enumerating and storing all the initial internal coordinate guesses
+    for i, guess in enumerate(init_guesses):
+        energy = mol.energy(guess)
+        init_energies.append(energy - start_energy)
+        logger.info(f'Evaluated initial guess {i+1}/{init_steps}. Energy-E0: {energy-start_energy}')
+
+        if out_dir is not None:
+            mol.write_xyz(f'{out_dir}/training_loop/InitialGuess_{i}.xyz')
+
+    init_coords_guesses = np.array([start_coords, *init_guesses.tolist()])
+    init_energy_guesses = [0.] + init_energies
+    
+    return init_coords_guesses, init_energy_guesses
--- boptimol/optimizer.py
+++ boptimol/optimizer.py
@@ -42,58 +42,50 @@ def run_optimization(mol: Molecule, n_steps: int,
                 'ediff': energy - start_energy
             })
         mol.write_xyz(f'{out_dir}/training_loop/{i}.xyz')
-
-        
-    # Evaluate initial point
-    start_coords = mol.parameters
-    start_energy = mol.energy(start_coords)
-    logger.info(f'Computed initial energy: {start_energy}')
-
+    
+    
     # Begin a structure log, if output available
     if out_dir is not None:
         log_path = out_dir.joinpath('structures.csv')
         with log_path.open('w') as fp:
             writer = DictWriter(fp, ['time', 'coords', 'energy', 'ediff'])
             writer.writeheader()
-
-        add_entry(start_coords, start_energy, 0)
-    
-    init_guesses = initial_guess(mol, init_steps)
-    init_energies = []
-    
-    for i, guess in enumerate(init_guesses):
-        energy = mol.energy(guess)
-        init_energies.append(energy - start_energy)
-        logger.info(f'Evaluated initial guess {i+1}/{init_steps}. Energy-E0: {energy-start_energy}')
-
-        if out_dir is not None:
-            add_entry(guess, energy, f'InitialGuess_{i}')
-
-    # Save the initial guesses
-    observed_coords = np.array([start_coords, *init_guesses.tolist()])
-    observed_energies = [0.] + init_energies
-
+        
+  
+    # Evaluate initial point
+    start_coords = mol.parameters
+    start_energy = mol.energy(start_coords)
+    logger.info(f'''Initial Coordinates: {start_coords} \n 
+                Computed initial energy: {start_energy}''')
     bounds = torch.tensor(mol.bounds, dtype=torch.float64)
 
-    # Loop over many steps
-    best_energy = np.min(observed_energies)
     
-    for step in range(n_steps):
-        # Make a new search space
-        next_coords = select_next_points_botorch(bounds, observed_coords, observed_energies)
+    # Guessing internal structure (Active Learning)
+    observed_coords, observed_energies = initial_guess(mol, init_steps, out_dir)
+    best_energy = np.min(observed_energies)
 
-        # Compute the energies of those points
-        energy = mol.energy(next_coords)
-        logger.info(f'Evaluated energy in step {step+1}/{n_steps}. Energy-E0: {energy-start_energy}')
+    
+    ''' Bayesian Optimization Loop '''
+    for step in range(n_steps):
         
+        # Make a new search space
+        next_coords = select_next_points_botorch(bounds, 
+                                                 observed_coords, 
+                                                 observed_energies)
+        
+        energy = mol.energy(next_coords)
+        logger.info(f'''Evaluated energy in step {step+1}/{n_steps}. 
+                    Energy-E0: {energy-start_energy}''')
+        
+        # Logging the structure
+        if out_dir is not None:
+            add_entry(next_coords, energy, f'Optim_{step+1}')
+        
+        # Logging the structure is it's the current best
         if energy - start_energy < np.min(observed_energies) and out_dir is not None:
             filename = out_dir.joinpath(f'/training_loop/best_{step}.xyz')
             mol.set_parameters(next_coords)
-            mol.write_xyz(f'{out_dir}/training_loop/best_{step}.xyz')
-
-        # Update the log
-        if out_dir is not None:
-            add_entry(next_coords, energy, f'Optim_{step+1}')
+            mol.write_xyz(f'{out_dir}/training_loop/best_{step+1}.xyz')
 
         # Update the search space
         observed_coords = np.vstack([observed_coords, next_coords])
@@ -107,13 +99,12 @@ def run_optimization(mol: Molecule, n_steps: int,
             logger.info('Converged!')
             break
 
+    # Saving the best observed coordinates and energies
     best_coords = observed_coords[np.argmin(observed_energies)]
     energy = mol.energy(best_coords)
-
-    # Final result
     logger.info(f'Finished in step {step+1}/{n_steps}. Energy-E0: {energy-start_energy}')
 
-    # Write the final result
+    # Writing the final result
     filename = out_dir.joinpath(f'best.xyz')
     mol.set_parameters(best_coords)
     mol.write_xyz(filename)
